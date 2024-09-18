94c4edeedafab0382857e21b4bbb7ffb7cb5b6c5
Pranandha Dhaiva <pranan.dha24@gmail.com>
2024-09-18 17:09:19 +0700

    Upload solutions

---
+++ src/ClassAndObject/ClassAndObject/OperasiMatematika/Kalkulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassAndObject.OperasiMatematika
+{
+    public class Kalkulator
+    {
+        public double Tambah(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Kurang(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Kali(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Bagi(double a, double b)
+        {
+            if (b == 0)
+            {
+                return double.PositiveInfinity;
+            }
+            return a / b;
+        }
+    }
+}
--- src/ClassAndObject/ClassAndObject/Program.cs
+++ src/ClassAndObject/ClassAndObject/Program.cs
@@ -1,26 +1,49 @@
-﻿namespace ClassAndObject
+﻿using System;
+using ClassAndObject.Perpustakaan;
+using ClassAndObject.Kendaraan;
+using ClassAndObject.TokoLaptop;
+using ClassAndObject.TokoSepeda;
+using ClassAndObject.OperasiMatematika;
+
+namespace ClassAndObject
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Buku
-           
+            Buku buku = new Buku("Laskar Pelangi", "Andrea Hirata", 2005);
+            buku.TampilkanInfo();
+
+            Console.WriteLine();
 
             // Kendaraan
+            Mobil mobil = new Mobil("Toyota", "Corolla", 2020);
+            mobil.TampilkanSpesifikasi();
 
-           
+            Console.WriteLine();
 
             // Laptop
-           
-           
+            Laptop laptop = new Laptop("Dell", "Intel Core i7", 16, 512);
+            laptop.TampilkanSpesifikasi();
+
+            Console.WriteLine();
+
             // Sepeda
-            
+            Sepeda sepeda = new Sepeda("Polygon", "Mountain Bike", 14.5);
+            sepeda.TampilkanInformasi();
+
+            Console.WriteLine();
 
             // Kalkulator
-            
-
-
+            Kalkulator kalkulator = new Kalkulator();
+            double a = 10;
+            double b = 5;
+            Console.WriteLine($"Penjumlahan: {a} + {b} = {kalkulator.Tambah(a, b)}");
+            Console.WriteLine($"Pengurangan: {a} - {b} = {kalkulator.Kurang(a, b)}");
+            Console.WriteLine($"Perkalian: {a} * {b} = {kalkulator.Kali(a, b)}");
+            Console.WriteLine($"Pembagian: {a} / {b} = {kalkulator.Bagi(a, b)}");
+            Console.WriteLine($"Pembagian dengan nol: {a} / 0 = {kalkulator.Bagi(a, 0)}");
         }
     }
 }
